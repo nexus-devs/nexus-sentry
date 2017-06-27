afd8a4187fc404232e6bd039335368f12840161c
Kaptard <kilian.volb@protonmail.com>
2017-06-27 07:32:46 +0200

    Prevent enqueueing the same player multiple times

--- index.js
+++ index.js
@@ -1,20 +1,22 @@
 const Sentry = require("./lib/sentry.js")
 const sentry = new Sentry()
 
- // Trade chat monitoring
- if (process.env.NEXUS_SENTRY_TASK === "monitor") {
-     sentry.monitor()
- }
+// Trade chat monitoring
+if (process.env.NEXUS_SENTRY_TASK === "monitor") {
+    sentry.monitor()
+}
 
- // Interact with game as requested via api.nexus-stats.com
- else if (process.env.NEXUS_SENTRY_TASK === "listen") {
-     sentry.listen()
- }
+// Interact with game as requested via api.nexus-stats.com
+else if (process.env.NEXUS_SENTRY_TASK === "listen") {
+    sentry.listen()
+}
 
 // Trigger user-stats fetcher directly
- else {
-     sentry.playerSentry.getPlayer("MrBubbleSS")
-     sentry.playerSentry.getPlayer("Tobiah")
-     sentry.playerSentry.getPlayer("Chungus")
-     sentry.playerSentry.getPlayer("Zilork")
- }
+else {
+    sentry.playerSentry.getPlayer("MrBubbleSS")
+    sentry.playerSentry.getPlayer("Tobiah")
+    sentry.playerSentry.getPlayer("Tobiah")
+    sentry.playerSentry.getPlayer("Tobiah")
+    sentry.playerSentry.getPlayer("Chungus")
+    sentry.playerSentry.getPlayer("Zilork")
+}
