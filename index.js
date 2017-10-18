f0bd35953f1338d2057c6348303695ba13796100
Kilian <kilian.volb@protonmail.com>
2017-10-18 08:13:19 +0200

    fix crash on empty item list response

--- index.js
+++ index.js
@@ -2,15 +2,18 @@ const Sentry = require("./lib/sentry.js")
 const sentry = new Sentry()
 
 process.on('unhandledRejection', err => {
-  throw err
+  console.log(err)
+})
+process.on('uncaughtException', err => {
+  console.log(err)
 })
 
 // Trade chat monitoring
 if (process.env.NEXUS_SENTRY_TASK === "monitor") {
-    sentry.monitor()
+  sentry.monitor()
 }
 
 // Interact with game as requested via api.nexus-stats.com
 else if (process.env.NEXUS_SENTRY_TASK === "listen") {
-    sentry.listen()
+  sentry.listen()
 }
